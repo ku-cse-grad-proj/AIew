2f2bbdcb6e74919308b3a837d02051643efd1622
lth-1026 <th20001026@gmail.com>
2025-07-28 16:59:46 +0900

    refactor(web-client): useLayouEffect를 이용해 랜더링될 때 accessToken 저장

--- apps/web-client/src/app/auth/callback/page.tsx
+++ apps/web-client/src/app/auth/callback/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
+import { useLayoutEffect } from 'react'
 
 export default function CallbackClient() {
   const searchParams = useSearchParams()
   const router = useRouter()
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const accessToken = searchParams.get('accessToken')
     if (accessToken) {
-      localStorage.setItem('access_token', accessToken)
+      localStorage.setItem('accessToken', accessToken)
       router.replace('/dashboard') // 홈으로 리디렉션
     }
   }, [searchParams, router])
 
-  return <p>로그인 처리 중입니다...</p>
+  return <></>
 }
