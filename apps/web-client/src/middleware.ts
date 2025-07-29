610cd28e6c6599042d95809c44518ce464c48a74
lth-1026 <th20001026@gmail.com>
2025-07-29 23:48:43 +0900

    feat(web-client): middleware를 이용해 refresh token 구현
    config에 설정된 주소의 Server Component 경우에는  해당 middleware를 거침
    
    만약 accessToken이 없다면 refresh token을 발급받고,
    성공하면 cookie를 새로 저장,
    실패하면 로그인 페이지로 redirect한다.

---
+++ apps/web-client/src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const access = req.cookies.get('accessToken')?.value
+
+  //accessToken이 없으면 refresh 요청
+  if (!access) {
+    const refreshRes = await fetch(`http://localhost:3000/auth/refresh`, {
+      method: 'POST',
+      headers: {
+        Cookie: req.headers.get('cookie') ?? '',
+      },
+    })
+
+    // refresh 성공 시 쿠키 설정
+    if (refreshRes.ok) {
+      const { accessToken } = await refreshRes.json()
+      const res = NextResponse.next()
+
+      res.cookies.set('accessToken', accessToken, {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+      })
+      return res
+    }
+
+    // refresh 실패 시 로그인 페이지로 리디렉션
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  return NextResponse.next()
+}
+
+//TODO: private 주소 한 파일로 변경해 관리하기 쉽도록 변경
+export const config = {
+  matcher: ['/dashboard/:path*', '/api/me', '/settings/:path*'],
+}
