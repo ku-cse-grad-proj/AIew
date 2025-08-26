94bf093574349696457b264e1735ede72de3e456
lth-1026 <th20001026@gmail.com>
2025-08-26 18:49:06 +0900

    refactor(web): header 위치를 root layout에서 가상의 공통 router인 (main)의 layout으로 변경

---
+++ apps/web-client/src/app/(main)/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+
+interface MainLayoutProps {
+  children: ReactNode
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  const mainBackground =
+    'bg-slate-400/20 rounded-[20px] shadow-[0px_8px_24px_0px_rgba(0,0,0,0.07)] border border-white/40'
+  return (
+    <div className="w-full h-dvh flex flex-col">
+      <header className="w-full max-w-1248 mx-auto h-96 flex justify-between items-center p-24">
+        <span>AIew</span>
+        <nav>dashboard interview setting</nav>
+        <img src="/profile.svg" alt="profile" />
+      </header>
+      <main className={`flex-1 w-full max-w-1296 mx-auto px-24 pb-24`}>
+        <div className={`w-full h-full ${mainBackground}`}>{children}</div>
+      </main>
+    </div>
+  )
+}
