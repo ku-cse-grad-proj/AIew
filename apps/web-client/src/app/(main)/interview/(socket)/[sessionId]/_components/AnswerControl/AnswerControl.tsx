7e81ee84c533f37ba82f839105b712d1b43f8055
lth-1026 <th20001026@gmail.com>
2025-09-23 05:35:36 +0900

    feat(web): 답변 시작하기 전 Redo 버튼 안보이게 변경

--- apps/web-client/src/app/(main)/interview/(socket)/[sessionId]/_components/AnswerControl/AnswerControl.tsx
+++ apps/web-client/src/app/(main)/interview/(socket)/[sessionId]/_components/AnswerControl/AnswerControl.tsx
@@ -13,10 +13,12 @@ export default function AnswerControl({ className }: { className?: string }) {
     <div
       className={`w-full h-full p-24 rounded-[20px] bg-neutral-card relative shadow-box ${className}`}
     >
-      <div className="w-full h-full flex items-center justify-between">
-        <RedoButton onClick={handleRedo} />
-        {startAt && <AnswerTimer />}
-      </div>
+      {startAt && (
+        <div className="w-full h-full flex items-center justify-between">
+          <RedoButton onClick={handleRedo} />
+          <AnswerTimer />
+        </div>
+      )}
       <AnswerButton
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         onClick={handleAnswer}
