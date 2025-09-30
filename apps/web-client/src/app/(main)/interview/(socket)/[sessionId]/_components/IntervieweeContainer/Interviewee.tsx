1f31cb72554f5f34027b33b2677499931bd0431d
lth-1026 <th20001026@gmail.com>
2025-09-30 16:59:37 +0900

    feat(web): socket으로 영상 송출

--- apps/web-client/src/app/(main)/interview/(socket)/[sessionId]/_components/IntervieweeContainer/Interviewee.tsx
+++ apps/web-client/src/app/(main)/interview/(socket)/[sessionId]/_components/IntervieweeContainer/Interviewee.tsx
@@ -2,11 +2,26 @@
 
 import { useRouter } from 'next/navigation'
 import { useEffect, useRef } from 'react'
+import { useShallow } from 'zustand/shallow'
+
+import { useAnswerStore } from '@/app/lib/answerStore'
+import { interviewSocket } from '@/app/lib/socket/interviewSocket'
 
 export default function Interviewee() {
   const videoRef = useRef<HTMLVideoElement | null>(null)
   const streamRef = useRef<MediaStream | null>(null)
+  const recordRef = useRef<MediaRecorder | null>(null)
+  const chunksRef = useRef<Blob[]>([])
   const router = useRouter()
+
+  //TODO:: redo일 때 어떻게 할지 고민할 것
+  const { startAt, endAt } = useAnswerStore(
+    useShallow((state) => ({
+      startAt: state.startAt,
+      endAt: state.endAt,
+      isRedo: state.isRedo,
+    })),
+  )
 
   useEffect(() => {
     const setupStream = async () => {
@@ -20,11 +35,18 @@ export default function Interviewee() {
       }
 
       if (streamRef.current) return
+
       try {
+        //화면에 보이는 곳은 30frame으로 송출
         const stream = await navigator.mediaDevices.getUserMedia({
-          video: { width: { ideal: 1280 }, height: { ideal: 720 } },
+          video: {
+            width: { ideal: 1280 },
+            height: { ideal: 720 },
+            frameRate: { ideal: 30, max: 30 },
+          },
         })
         streamRef.current = stream
+        //video component와 연결
         if (videoRef.current) {
           videoRef.current.srcObject = stream
           await videoRef.current.play().catch(() => {})
@@ -62,6 +84,86 @@ export default function Interviewee() {
       }
     }
   }, [])
+
+  async function startRec() {
+    if (!streamRef.current) {
+      console.error('stream이 존재하지 않습니다')
+      return
+    }
+
+    //기존 stream을 복제해 1frame로 변환
+    const v = streamRef.current.getVideoTracks()[0]
+    const recVideo = v.clone()
+    await recVideo.applyConstraints({
+      width: 1280,
+      height: 720,
+      frameRate: { ideal: 1, max: 1 },
+    }) // 또는 1fps
+    const recStream = new MediaStream([recVideo])
+    const rec = new MediaRecorder(recStream) //브라우저 default 값으로 mimeType 설정
+
+    let index = 0
+
+    rec.ondataavailable = (e) => {
+      if (e.data && e.data.size > 0) {
+        chunksRef.current.push(e.data)
+        console.log(e.data.size)
+        interviewSocket.emit('client:upload-chunk', {
+          chunk: e.data,
+          index: index++,
+        })
+      }
+    }
+
+    rec.onstop = async () => {
+      try {
+        const fullBlob = new Blob(chunksRef.current, { type: rec.mimeType })
+        const filename = `record-${crypto.randomUUID()}`
+        const file = new File([fullBlob], filename, {
+          type: rec.mimeType,
+          lastModified: Date.now(),
+        })
+
+        interviewSocket.emit('client:upload-finish', { type: rec.mimeType })
+
+        //file download
+        // const url = URL.createObjectURL(file)
+        // const a = document.createElement('a')
+        // a.href = url
+        // a.download = filename
+        // a.style.display = 'none'
+        // document.body.appendChild(a)
+        // a.click()
+
+        console.log(file)
+        chunksRef.current = []
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    recordRef.current = rec
+
+    rec.start(1000)
+  }
+
+  async function stopRec() {
+    if (recordRef.current) {
+      recordRef.current.stop()
+    }
+  }
+
+  useEffect(() => {
+    const handleRecording = async () => {
+      if (startAt && !endAt) {
+        await stopRec()
+        startRec()
+      } else {
+        await stopRec()
+      }
+    }
+    handleRecording()
+  }, [startAt, endAt])
   return (
     <video
       ref={videoRef}
