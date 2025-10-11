8319b73b1d3f4b19137e8de1c59ca36185c12b06
lth-1026 <th20001026@gmail.com>
2025-10-11 23:12:13 +0900

    feat(web): overview ui 구현

--- apps/web-client/src/app/(main)/reports/_components/OverviewSection.tsx
+++ apps/web-client/src/app/(main)/reports/_components/OverviewSection.tsx
@@ -1,7 +1,8 @@
+import MetricsPannel from './MetricsPannel'
+
 export default function OverviewSection({ className }: { className?: string }) {
   const dtStyle = 'text-[14px] text-neutral-subtext'
-  const cardStyle =
-    'w-full h-full rounded-[10px] bg-neutral-background transition-transform transition-shadow duration-300 ease-in-out hover:shadow-box hover:scale-[1.03]'
+
   return (
     <section className={`w-full h-full flex flex-col py-16 px-24 ${className}`}>
       <header className="pb-24">
@@ -30,12 +31,7 @@ export default function OverviewSection({ className }: { className?: string }) {
             </dd>
           </div>
         </dl>
-        <div className="flex-1 h-full grid grid-rows-2 grid-cols-2 gap-24">
-          <button className={cardStyle}>score</button>
-          <button className={cardStyle}>duration</button>
-          <button className={cardStyle}>date</button>
-          <button className={cardStyle}>question count</button>
-        </div>
+        <MetricsPannel className="flex-1 min-w-0 h-full" />
       </div>
     </section>
   )
