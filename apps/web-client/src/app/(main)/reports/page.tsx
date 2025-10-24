4ce908226794f5e42a183e122949d2e1a5c61f62
lth-1026 <th20001026@gmail.com>
2025-10-25 02:10:17 +0900

    feat(web): reprot calendar 컴포넌트 추가

--- apps/web-client/src/app/(main)/reports/page.tsx
+++ apps/web-client/src/app/(main)/reports/page.tsx
@@ -1,15 +1,19 @@
+import ReportCalendarButton from './_components/ReportCalendarButton'
 import ReportSearchInput from './_components/ReportSearchInput'
 import ReportSelect from './_components/ReportSelect'
 
 export default function ReportsPage() {
   return (
     <article>
-      <div className="flex gap-8">
-        <ReportSelect name="search_section" defaultValue={'title'}>
-          <option value="title">title</option>
-          <option value="company">company</option>
-        </ReportSelect>
-        <ReportSearchInput />
+      <div className="flex justify-between">
+        <div className="flex gap-8">
+          <ReportSelect name="search_section" defaultValue={'title'}>
+            <option value="title">title</option>
+            <option value="company">company</option>
+          </ReportSelect>
+          <ReportSearchInput />
+        </div>
+        <ReportCalendarButton />
       </div>
     </article>
   )
