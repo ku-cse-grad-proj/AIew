002dd94969fa69657f9c2b8ac2468a66a18f5294
lth-1026 <th20001026@gmail.com>
2025-10-25 16:03:06 +0900

    feat(web): select css 변경

--- apps/web-client/src/app/(main)/reports/_components/header/ReportSelect.tsx
+++ apps/web-client/src/app/(main)/reports/_components/header/ReportSelect.tsx
@@ -3,12 +3,12 @@ import { SelectHTMLAttributes } from 'react'
 export default function ReportSelect(
   props: SelectHTMLAttributes<HTMLSelectElement>,
 ) {
-  const view = 'w-120 h-40 px-8 border border-gray-300 rounded-[10px]'
+  const view = 'w-full h-40 px-8 border border-gray-300 rounded-[10px]'
   const textDecoration = 'text-neutral-text'
   return (
     <select
       {...props}
-      className={`${view} ${textDecoration} ${props.className}`}
+      className={`${props.className} ${view} ${textDecoration}`}
     >
       {props.children}
     </select>
