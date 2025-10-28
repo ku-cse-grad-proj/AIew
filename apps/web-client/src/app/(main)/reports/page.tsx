d3d6917316e991a2908586c0c573fe35a7fd7b9f
lth-1026 <th20001026@gmail.com>
2025-10-29 02:34:33 +0900

    feat(web): pagination을 위해 전체 page수 호출
    test를 위해 route 이용
    mock-api에 한해 인증 검사 제외

--- apps/web-client/src/app/(main)/reports/page.tsx
+++ apps/web-client/src/app/(main)/reports/page.tsx
@@ -2,9 +2,21 @@ import ReportHeader from './_components/header/ReportHeader'
 import Pagination from './_components/pagination/Pagination'
 import ReportTable from './_components/table/ReportTable'
 
-export default async function ReportsPage() {
-  // const totalPages = await fetchReportPages(query)
-  const totalPages = 49
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+type Query = [string, string][]
+
+export default async function ReportsPage({
+  searchParams,
+}: {
+  searchParams: SearchParams
+}) {
+  const params = await searchParams
+  //URLSearchParmas의 생성자 type에 parmas 맞게 변환
+  const query = Object.entries(params).filter(
+    (_, value) => value != null,
+  ) as Query
+  const totalPages = await fetchReportsCount(query)
+
   return (
     <article className="w-full h-full flex flex-col items-center gap-24">
       <ReportHeader />
@@ -12,4 +24,13 @@ export default async function ReportsPage() {
       {totalPages > 1 && <Pagination totalPages={totalPages} />}
     </article>
   )
+}
+
+async function fetchReportsCount(query: Query): Promise<number> {
+  const response = await fetch(
+    `http://localhost:4000/mock-api/reports/pages/count?${new URLSearchParams(query)}`,
+  )
+  const data = await response.json()
+
+  return data
 }
