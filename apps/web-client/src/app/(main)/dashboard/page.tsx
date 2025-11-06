6db0fecfe6144b9ac477ca5ee22ca1a55e92d3d5
lth-1026 <th20001026@gmail.com>
2025-11-06 22:05:53 +0900

    feat(web): dashboard 기본 구조 구현
    tailwind의 lg(1024px) 기준으로 layout을 달리함.
    grid를 이용해 구조 구현
    공통 card css는 dashboard.module.css에서 정의함.
    card css에 색상을 지정하면 외부에서 색상을 못 지정하기에 색상은 정의하지 않음

--- apps/web-client/src/app/(main)/dashboard/page.tsx
+++ apps/web-client/src/app/(main)/dashboard/page.tsx
@@ -1,3 +1,31 @@
+import styles from './_components/dashboard.module.css'
+import CardSection from './CardSection'
+
 export default function Dashboard() {
-  return <>dashboard</>
+  return (
+    <article className="w-full h-full flex flex-col">
+      <h1 className="text-[32px] font-bold leading-[48px]">
+        Taeho's Dashboard
+      </h1>
+      <div className="flex-1 w-full min-h-0 grid grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 gap-24 pt-24">
+        <section className="w-full h-full grid grid-cols-2 grid-rows-2 gap-24">
+          <div className={`h-full col-span-2 ${styles.card}`}>name</div>
+          <div className={`h-full  bg-neutral-gray ${styles.card}`}>
+            interviewed count
+          </div>
+          <div className={`h-full ${styles.card} bg-secondary`}>user info</div>
+        </section>
+        <CardSection className="">recent interview</CardSection>
+        <CardSection className="bg-neutral-gray order-2 lg:order-none">
+          recent reports
+        </CardSection>
+        <CardSection className="lg:col-span-2 lg:row-start-2 row-start-3 col-span-2">
+          recent's result
+        </CardSection>
+        <CardSection className="order-1 lg:order-none">
+          interviewed company
+        </CardSection>
+      </div>
+    </article>
+  )
 }
