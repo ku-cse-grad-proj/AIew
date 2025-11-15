c4c5abcbebc96a00c77eb68811e00c817d67e01a
lth-1026 <th20001026@gmail.com>
2025-11-15 16:42:46 +0900

    refactor(web): login page에 connection()함수 추가
    기존 사용되던 force-dynamic 선언문을 사용 못하게 됨.
    하지만 동적으로 환경변수를 받아야 하기에 connection()이 될 때까지 기다린 후
    env의 값을 할당받음

--- apps/web-client/src/app/login/page.tsx
+++ apps/web-client/src/app/login/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react'
+
 import Github from '../../../public/github.svg'
 import Google from '../../../public/google.svg'
 
@@ -8,12 +10,14 @@ export default function Login() {
     <div className="h-full flex flex-col justify-center items-center gap-48">
       <span className="text-4xl text-bold font-extrabold">welcome</span>
       <div className="flex flex-col gap-24">
-        <LoginButton text="Sign in with Google" link="google">
-          <Google width={25} height={25} />
-        </LoginButton>
-        <LoginButton text="Sign in with Github" link="github">
-          <Github width={25} height={25} />
-        </LoginButton>
+        <Suspense>
+          <LoginButton text="Sign in with Google" link="google">
+            <Google width={25} height={25} />
+          </LoginButton>
+          <LoginButton text="Sign in with Github" link="github">
+            <Github width={25} height={25} />
+          </LoginButton>
+        </Suspense>
       </div>
     </div>
   )
