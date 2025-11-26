23e8acf9e9613334a66b9be7d0eac66b38376a5a
lth-1026 <th20001026@gmail.com>
2025-11-26 16:22:38 +0900

    fix(web): interview 관련 반응형 수정

--- apps/web-client/src/app/(main)/interview/(socket)/waiting/[sessionId]/components/InfoItem.tsx
+++ apps/web-client/src/app/(main)/interview/(socket)/waiting/[sessionId]/components/InfoItem.tsx
@@ -8,11 +8,11 @@ export default function InfoItem({
   className?: string
 }) {
   return (
-    <div className={`flex-1 min-h-0 flex flex-col ${className}`}>
+    <div className={`flex-1 max-h-60 flex flex-col ${className}`}>
       <dt className="text-neutral-subtext text-[16px] leading-[24px]">
         {label}
       </dt>
-      <div className="flex-1 min-h-0 overflow-auto">
+      <div className="flex-1 min-h-36 overflow-auto">
         <dd className="text-[24px] leading-[36px]">{value}</dd>
       </div>
     </div>
