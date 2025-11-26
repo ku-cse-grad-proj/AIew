4ca2b991ff8cd9de5785d55e2d75fea49d14b313
lth-1026 <th20001026@gmail.com>
2025-11-26 16:37:34 +0900

    feat(web): reports/graph api 적용

--- apps/web-client/src/app/(main)/reports/_lib/api.ts
+++ apps/web-client/src/app/(main)/reports/_lib/api.ts
@@ -50,26 +50,18 @@ export async function getReports(query: Query) {
 }
 
 export async function getReportsGraph(query: Query) {
-  // const { CORE_API_URL, API_PREFIX } = process.env
+  const { CORE_API_URL, API_PREFIX } = process.env
 
-  // const response = await privateFetch(
-  //   `${CORE_API_URL}/${API_PREFIX}/reports/graphs?${new URLSearchParams(
-  //     query,
-  //   )}`,
-  //   { cache: 'force-cache', next: { tags: [CACHE_TAG.REPORTS] } },
-  // )
+  const response = await privateFetch(
+    `${CORE_API_URL}/${API_PREFIX}/reports/graph?${new URLSearchParams(query)}`,
+    { cache: 'force-cache', next: { tags: [CACHE_TAG.REPORTS] } },
+  )
 
-  // if (!response.ok) {
-  //   throw new Error('Reports graph 조회 중 문제가 발생했습니다.')
-  // }
-  console.log('getReportsGraph called with query:', query)
-  return {
-    labels: ['토스', '카카', '배달의 민족', '네이버', '쿠팡'],
-    scores: [3.4, 2, 0, 5, 3.1],
-    durations: [2, 4, 5, 7, 10],
+  if (!response.ok) {
+    throw new Error('Reports graph 조회 중 문제가 발생했습니다.')
   }
 
-  // return await response.json()
+  return await response.json()
 }
 
 export async function getReport(id: string): Promise<ReportResponse> {
