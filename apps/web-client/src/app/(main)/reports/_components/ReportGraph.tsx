961931322d3f2785271de88e5e90b2c940844bb9
lth-1026 <th20001026@gmail.com>
2025-11-26 17:25:16 +0900

    feat(web): graph 값 없을 때 Empty 화면 보여주기

--- apps/web-client/src/app/(main)/reports/_components/ReportGraph.tsx
+++ apps/web-client/src/app/(main)/reports/_components/ReportGraph.tsx
@@ -1,4 +1,5 @@
 import LineGraph from '../../_components/graph/LineGraph'
+import EmptyMessage from '../../dashboard/_components/EmptyMessage'
 import { graphData } from '../_types'
 
 export default function ReportGraph({
@@ -16,7 +17,17 @@ export default function ReportGraph({
   ]
   return (
     <section className={`w-full h-full px-8 pt-8 pb-32 ${className}`}>
-      <LineGraph data={graphData} />
+      {graphData ? (
+        <LineGraph data={graphData} />
+      ) : (
+        <div className="w-full h-full flex items-center justify-center">
+          <EmptyMessage
+            main=" No result to display"
+            sub="complete an interview to generate a report"
+            showIcon
+          />
+        </div>
+      )}
       <h2 className="absolute bottom-0 pl-16 pb-10 font-medium">graph</h2>
     </section>
   )
